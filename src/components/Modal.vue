96ba874b339d342de1d027a26efd6b4ab844df5d
chenzd123456 <chenzd123456@gmail.com>
2023-05-29 16:07:49 +0800

    增加注释

--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -1,60 +1,73 @@
 <template>
+    <!-- 使用Teleport从当前组件移入到body根元素下 -->
     <Teleport to="body">
-        <div class="overlay" v-show="active" @click.stop="emitClose">
-            <div class="modal">
-                <slot></slot>
-                <button @click="emitClose">Close</button>
-            </div>
+      <!-- 蒙层，用于遮盖原本内容 -->
+      <div class="overlay" v-show="active" @click.stop="emitClose">
+        <!-- 模态框 -->
+        <div class="modal">
+          <!-- 显示用户传递的内容，用于展示模态框的具体信息 -->
+          <slot></slot>
+          <!-- 用于关闭模态框的按钮 -->
+          <button @click="emitClose">Close</button>
         </div>
+      </div>
     </Teleport>
-</template>
-
-<script setup>
-import { onMounted, onUnmounted } from 'vue'
-
-const props = defineProps({ active: false });
-const emit = defineEmits(["close"]);
-
-function emitClose() {
+  </template>
+  
+  <script setup>
+  import { onMounted, onUnmounted } from 'vue'
+  
+  // 定义控制模态框是否展示的prop属性
+  const props = defineProps({
+    active: false,
+  });
+  
+  // 定义close事件的函数
+  const emit = defineEmits(["close"]);
+  
+  function emitClose() {
+    // 调用close事件，传递给父组件进行处理
     emit('close');
-}
-
-function onEscDown(event) {
-    if (event.keyCode === 27) {
-        emitClose()
+  }
+  
+  function onEscDown(event) {
+    if (event.keyCode === 27) { // ESC键的keyCode是27
+      emitClose(); // 发射关闭事件
     }
-}
-
-// 在组件 mount 时注册按键事件的监听器
-onMounted(() => {
+  }
+  
+  // 在组件 mount 时注册按键事件的监听器
+  onMounted(() => {
     window.addEventListener('keydown', onEscDown)
-})
-// 在组件 unmount 时移除按键事件的监听器
-onUnmounted(() => {
+  })
+  
+  // 在组件 unmount 时移除按键事件的监听器
+  onUnmounted(() => {
     window.removeEventListener('keydown', onEscDown)
-})
-
-</script>
-
-<style scoped>
-.overlay {
-    position: fixed;
-    top: 0;
-    left: 0;
-    width: 100%;
-    height: 100%;
-    background-color: rgba(0, 0, 0, 0.5);
-    display: flex;
-    align-items: center;
-    justify-content: center;
-}
-
-.modal {
-    position: relative;
-    z-index: 999;
-    background-color: white;
-    padding: 20px;
-    border-radius: 8px;
-    box-shadow: 0 0 10px rgba(0, 0, 0, 0.5);
-}
-</style>
+  })
+  
+  </script>
+  
+  <style scoped>
+  .overlay {
+    position: fixed; /* 创建一个绝对定位的元素 */
+    top: 0; /* 距离顶部为0 */
+    left: 0; /* 距离左侧为0 */
+    width: 100%; /* 占据100%的横向空间 */
+    height: 100%; /* 占据100%的纵向空间 */
+    background-color: rgba(0, 0, 0, 0.5); /* 半透明黑色背景 */
+    display: flex; /* 设置为flex布局 */
+    align-items: center; /* 垂直居中 */
+    justify-content: center; /* 水平居中 */
+  }
+  
+  .modal {
+    position: relative; /* 相对定位 */
+    z-index: 999; /* 设置在最上层 */
+    background-color: white; /* 白色背景 */
+    padding: 20px; /* 内部的边距为20px */
+    border-radius: 8px; /* 圆角为8px */
+    box-shadow: 0 0 10px rgba(0, 0, 0, 0.5); /* 添加阴影效果 */
+  }
+  </style>
+  
